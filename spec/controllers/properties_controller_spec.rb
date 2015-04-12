4d54be7be289e6e52d494b3a52e7cd10c09fa436
Jordan <jordan@51shepherd.com>
2015-04-12 22:07:59 +0800

    spec show

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PropertiesController, type: :controller do
+  let(:property){ create(:property) }
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: property.id
       expect(response).to have_http_status(:success)
     end
   end
