08b4e2c349630bddd62bb0a0cdaf81d880dd1785
Philip L Hallstrom <philip@practicalstrategies.com>
2015-04-14 13:22:59 -0700

    deploy

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,7 @@ module Homey
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.assets.version = "1.0"
   end
 end
