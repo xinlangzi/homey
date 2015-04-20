badcd9d68b22da78179f8b65e488da3bffda9c12
Philip L Hallstrom <philip@practicalstrategies.com>
2015-04-20 12:54:30 -0700

    revert to amazon

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    # address: "email-smtp.us-west-2.amazonaws.com",
-    address: "smtp.zoho.com",
+    address: "email-smtp.us-west-2.amazonaws.com",
+    # address: "smtp.zoho.com",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
