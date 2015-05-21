5e17e38519667ae6f6004b6582dc7113a802923f
Philip L Hallstrom <philip@practicalstrategies.com>
2015-05-21 15:52:52 -0700

    weather specs

--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -3,7 +3,10 @@ describe Weather do
     it "should fetch the weather" do
       VCR.use_cassette("weather") do
         Weather.fetch
-        ap Weather.default
+        weather = Weather.default
+        expect(weather.temp_c).to eq(19)
+        expect(weather.temp_f).to eq(67)
+        expect(weather.icon).to eq("10n")
       end
     end
   end
