f2d8793bc66cdc9b22585f00a56af3b8f171743d
Jordan <jordan@51shepherd.com>
2015-05-23 11:05:40 +0800

    decorate layout

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -7,7 +7,7 @@ class Weather < ActiveRecord::Base
     temp = my_hash["main"]["temp"].to_f
     default.update_attributes!(temp_c: temp.round, temp_f: (temp * 1.8 + 32).round, icon: my_hash["weather"].first["icon"])
   end
-  
+
   def self.default
     Weather.first || init
   end
