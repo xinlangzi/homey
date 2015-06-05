b77113a6db92500b2a1398be28796871d1d8e6af
Jordan <jordan@51shepherd.com>
2015-06-05 17:50:51 +0800

    fixed color

--- app/uploaders/watermark_uploader.rb
+++ app/uploaders/watermark_uploader.rb
@@ -2,7 +2,7 @@
 
 class WatermarkUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20150603202715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "areas", force: :cascade do |t|
     t.string   "name",        null: false
